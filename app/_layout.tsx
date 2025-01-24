ef64a57b70dd5f439f584cce102e9708c2821bba
nadersafadev <nader@orthoplexsolutions.com>
2025-01-24 18:26:37 +0200

    added react-native-bottom-tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,19 @@
-import { Slot } from 'expo-router'
+import { tokenCache } from '@/cache'
 import {
   ClerkLoaded,
   ClerkProvider,
   SignedIn,
   SignedOut,
-  useAuth,
 } from '@clerk/clerk-expo'
-import * as SecureStore from 'expo-secure-store'
-import { Stack } from 'expo-router'
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from '@react-navigation/native'
+import { Redirect, Stack } from 'expo-router'
 import { useColorScheme } from 'react-native'
-import { Redirect } from 'expo-router'
 
 const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
-
-const tokenCache = {
-  async getToken(key: string) {
-    try {
-      return SecureStore.getItemAsync(key)
-    } catch (err) {
-      return null
-    }
-  },
-  async saveToken(key: string, value: string) {
-    try {
-      return SecureStore.setItemAsync(key, value)
-    } catch (err) {
-      return
-    }
-  },
-}
 
 export default function RootLayout() {
   const colorScheme = useColorScheme()
